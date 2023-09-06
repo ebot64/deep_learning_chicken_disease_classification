f5b509866ce589ad9e994b1598dfae6602cc6844
Tobechukwu Ezenachukwu <ebot64@yahoo.com>
2023-09-06 07:37:58 +0100

    Resolve ModuleNotFound Error on main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from cnnClassifier import logger
-from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
-from cnnClassifier.constants import *
-from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from src.cnnClassifier.constants import *
+from src.cnnClassifier.utils.common import read_yaml, create_directories
+from src.cnnClassifier.entity.config_entity import DataIngestionConfig
 
 
 class ConfigurationManager:
